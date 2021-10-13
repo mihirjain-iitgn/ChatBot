dc1d0448a00d20b0a396f313dc2c30e5f46a7850
Mihir Jain <67909784+mihirjain-iitgn@users.noreply.github.com>
2021-10-13 14:09:09 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,27 +1,19 @@
 from flask import Flask, render_template, request
-
 import nltk
 from nltk.stem import WordNetLemmatizer
-lemmatizer = WordNetLemmatizer()
 import pickle
 import numpy as np
-from tensorflow import keras 
+import keras
 from keras.models import load_model
-model = load_model('chatbot_model.h5')
 import json
 import random
-intents = json.loads(open('intents.json').read())
-words = pickle.load(open('words.pkl','rb'))
-classes = pickle.load(open('classes.pkl','rb'))
-curr_context = ""
 
 def clean_up_sentence(sentence):
     sentence_words = nltk.word_tokenize(sentence)
     sentence_words = [lemmatizer.lemmatize(word.lower()) for word in sentence_words]
     return sentence_words
 
-
-def bow(sentence, words, show_details=True):
+def bow(sentence, words, show_details = True):
     sentence_words = clean_up_sentence(sentence)
     bag = [0]*len(words)  
     for s in sentence_words:
@@ -33,7 +25,7 @@ def bow(sentence, words, show_details=True):
     return(np.array(bag))
 
 def predict_class(sentence, model):
-    p = bow(sentence, words,show_details=False)
+    p = bow(sentence, words,show_details = False)
     res = model.predict(np.array([p]))[0]
     ERROR_THRESHOLD = 0.25
     results = [[i,r] for i,r in enumerate(res) if r>ERROR_THRESHOLD]
@@ -58,6 +50,14 @@ def chatbot_response(msg):
     return res
 
 app = Flask(__name__)
+nltk.download('punkt')
+nltk.download('wordnet')
+lemmatizer = WordNetLemmatizer()
+model = load_model('chatbot_model.h5')
+intents = json.loads(open('intents.json').read())
+words = pickle.load(open('words.pkl','rb'))
+classes = pickle.load(open('classes.pkl','rb'))
+
 @app.route("/")
 def main():
     return render_template("home.html")
@@ -68,5 +68,4 @@ def get_bot_response():
     res = chatbot_response(userText)
     return res
 
-if __name__ == "_main_":
-    app.run(debug=True)
+app.run(debug=True)
